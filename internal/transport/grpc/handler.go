7efd4247894c19c9146536e570009b6767d56037
elijah <cuiyeqing960904@gmail.com>
2023-03-21 11:13:18 -0400

    handling grpc server

--- internal/transport/grpc/handler.go
+++ internal/transport/grpc/handler.go
@@ -1,1 +1,14 @@
 package grpc
+
+type RocketService struct {
+}
+
+type Handler struct {
+	RocketService RocketService
+}
+
+func New(rktService RocketService) Handler {
+	return Handler{
+		RocketService: rktService,
+	}
+}
